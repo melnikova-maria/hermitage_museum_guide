e3f085874ab5060779d80c246cd6e9849f6c71ad
Мария Мельникова <mariamelnikova@MacBook-Pro.local>
2023-07-24 20:56:01 +0300

    merge_test

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,12 +4,11 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.home, name='main'),
-    path('about/', views.about, name = 'about'),
-    path('contacts/', views.contacts, name = 'contacts'),
-    path('login/', views.login, name = 'login'),
-    path('rooms/new_hermitage', views.new_hermitage, name = 'new_hermitage'),
-    path('rooms/old_hermitage', views.old_hermitage, name = 'old_hermitage'),
-    path('rooms/winter_palace', views.winter_palace, name = 'winter_palace'),
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('rooms/new_hermitage', views.new_hermitage, name='new_hermitage'),
+    path('rooms/old_hermitage', views.old_hermitage, name='old_hermitage'),
+    path('rooms/winter_palace', views.winter_palace, name='winter_palace'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
